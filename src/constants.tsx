ec0ec8cb998d18f13e105875acf3f00f6ea451a1
SergeyKazanskiy <87529752+SergeyKazanskiy@users.noreply.github.com>
2021-07-16 15:18:47 +0300

    Initial commit

---
+++ src/constants.tsx
@@ -0,0 +1,13 @@
+export const typeAction = {
+  ADD: "ADD",
+  REMOVE: "REMOVE",
+  MOVE: "MOVE",
+  EDIT: "EDIT"
+};
+
+export const typeMove = {
+  UP: "UP",
+  DOWN: "DOWN",
+  RIGHT: "RIGHT",
+  LEFT: "LEFT"
+};
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { render } from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+
+render(<App />, document.getElementById("root"));
